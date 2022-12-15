be3efe95308f23d315250932080dd9b0c6319baa
mandian <mandian@users.noreply.github.com>
2022-12-15 17:01:52 +0000

    use gitlab in chech-updates.sh

--- check-update.sh
+++ check-update.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-curl "https://github.com/BelledonneCommunications/belr/tags" 2>/dev/null |grep "tag/" |sed -e 's,.*tag/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
+#curl -s "https://github.com/BelledonneCommunications/belr/tags" |grep "tag/" |sed -e 's,.*tag/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
+curl -s "https://gitlab.linphone.org/BC/public/belr/-/tags" |grep "tags/" |sed -e 's,.*tags/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
 
